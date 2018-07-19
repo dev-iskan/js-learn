9e877e7eda71770782ad34f9aef65eceb57eea42
dev-iskan <successfuliskan@gmail.com>
2018-07-19 08:02:09 +0500

    mouse event finished

--- mouse-event/js/script.js
+++ mouse-event/js/script.js
@@ -1,82 +1,96 @@
 window.onload = function(e) {
 
-    function handler(e) {
-
-        e = e || window.event;
-
-        if (this.tagName == 'SPAN') {
-            this.style.backgroundColor = "yellow";
-            //e.cancelBubble = true;
-        } else if (this.tagName == 'P') {
-            this.style.backgroundColor = "green";
-        } else {
-            this.style.backgroundColor = "gray";
-        }
-
-    }
-
-    function addEvent(target, type, hendler) {
+   /* function addEvent(target, type, handler) {
         if (target.addEventListener) {
-            target.addEventListener(type, hendler, false)
+            target.addEventListener(type, handler, false)
         } else {
             target.attachEvent("on" + type, function(e) {
-                return hendler.call(target, e);
+                return handler.call(target, e);
             });
         }
     }
 
-    //addEvent(button,'click',handler);
+    function handler(e) {
 
-     document.getElementById('one').addEventListener('click', function(e){
-            this.style.backgroundColor = "grey";    
-            alert(this.tagName);
-            if(e.target.tagName == 'SPAN'){
-                stop;
-            }
-     }, true);
-
-            document.getElementById('two').addEventListener('click', function(e){
-            this.style.backgroundColor = "green";   
-            alert(this.tagName);
-     }, true);
-
-                document.getElementById('three').addEventListener('click', function(e){
-            this.style.backgroundColor = "yellow";  
-            alert(this.tagName);
-     }, true);
-
-
-
-    /*    document.getElementById('firstForm').addEventListener('click', function(e){
-            e.target.style.border = "1px solid red";
-
-            console.log(this);
-        }, false);*/
-
-
-   /* document.getElementById('one').addEventListener('click', function(e) {
-        this.style.backgroundColor = "grey";
-        alert(this.tagName);
-    }, false);
-
-    document.getElementById('two').addEventListener('click', function(e) {
+        e = e || window.event;
+        // console.log(e.clientX);
+        // console.log(e);
         this.style.backgroundColor = "green";
-        alert(this.tagName);
+
+    }
+
+    function handler2(e) {
+
+        e = e || window.event;
+        this.style.backgroundColor = "red";
+
+    }*/
+    // addEvent(document.getElementById('one'),'click', handler);
+    // addEvent(document.getElementById('one'),'dblclick', handler);
+    // addEvent(document.getElementById('one'),'mousemove', handler);
+    // addEvent(document.getElementById('one'),'mouseup', handler);
+    // addEvent(document.getElementById('one'),'mousedown', handler2);
+
+    // addEvent(document.getElementById('one'),'mouseover', handler);
+    // addEvent(document.getElementById('one'),'mouseout', handler2);
+
+    // addEvent(document.getElementById('one'), 'mouseenter', handler);
+    // addEvent(document.getElementById('one'), 'mouseleave', handler2);
+
+    function getScroll(w) {
+        w = w || window;
+
+        if (w.pageXOffset != null) {
+            return { x: w.pageXOffset, y: w.pageYOffset }
+        }
+    }
+
+    function moveObj(el, event) {
+        var scroll = getScroll();
+
+        var startX = event.clientX + scroll.x;
+        var startY = event.clientY + scroll.y;
+
+        var elX = el.offsetLeft;
+        var elY = el.offsetTop;
+
+        var deltaX = startX - elX;
+        var deltaY = startY - elY;
+
+        if (document.addEventListener) {
+            document.addEventListener('mousemove', moveHandler, true);
+            document.addEventListener('mouseup', upHandler, true);
+        }
+
+        if (event.stopPropagation) {
+            event.stopPropagation();
+        }
+        if (event.preventDefault) {
+            event.preventDefault();
+        }
+
+        function moveHandler(e) {
+            var scroll = getScroll();
+            el.style.left = (e.clientX + scroll.x - deltaX) + 'px';
+            el.style.top = (e.clientY + scroll.x - deltaY) + 'px';
+
+            if (e.stopPropagation) {
                 e.stopPropagation();
-    }, false);
+            }
+        }
 
-    document.getElementById('three').addEventListener('click', function(e) {
-        this.style.backgroundColor = "yellow";
-        alert(this.tagName);
-    }, false);
+        function upHandler(e) {
+            if (document.removeEventListener) {
+                document.removeEventListener("mouseup", upHandler, true);
+                document.removeEventListener("mousemove", moveHandler, true);
+            }
 
-    document.getElementById('two').addEventListener('click', function(e) {
-        this.style.backgroundColor = "green";
-        alert(this.tagName);
-    }, false);
-    document.getElementById('two').addEventListener('click', function(e) {
-        this.style.backgroundColor = "green";
-        alert(this.tagName);
-    }, false);*/
+            if (e.stopPropagation) {
+                e.stopPropagation();
+            }
+        }
+
+    }
+
 
 }
