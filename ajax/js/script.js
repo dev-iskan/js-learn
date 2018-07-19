c372faeb551364138771b66d1deee7d9cf7442c2
dev-iskan <successfuliskan@gmail.com>
2018-07-19 23:10:36 +0500

    ajax finished

--- ajax/js/script.js
+++ ajax/js/script.js
@@ -15,7 +15,18 @@ window.onload = function(e) {
         e.preventDefault();
         var request = new XMLHttpRequest();
         request.open("POST", 'server.php');
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        request.setRequestHeader('MyHead', 'someString');
+        request.send("name=BEN&age=18");
+        
+        request.onreadystatechange = function (){
+            if(request.readyState == 4 && request.status == 200){
+                document.getElementById('three').innerHTML = request.responseText;
+            }
+        }
     }
+
+
 
     var button = document.getElementById('myButton');
     addEvent(button, 'click', handler);
