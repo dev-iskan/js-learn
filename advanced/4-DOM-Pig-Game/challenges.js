f174c54ea9f3496bdd916081bfae9276c8ae4d42
dev-iskan <successfuliskan@gmail.com>
2018-09-21 18:17:02 +0500

    pig roll game-advanced

--- advanced/4-DOM-Pig-Game/challenges.js
+++ advanced/4-DOM-Pig-Game/challenges.js
@@ -9,10 +9,8 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, gamePlaying;
+var scores, roundScore, activePlayer, gamePlaying, lastDice;
 init();
-
-
 
 //document.querySelector('#current-'+ activePlayer).textContent = dice
 // document.querySelector('#current-'+ activePlayer).innerHTML = '<em>' + dice  + '</em>'
@@ -21,19 +19,39 @@ init();
 document.querySelector('.btn-roll').addEventListener('click', function() {
   if(gamePlaying) {
     //1. Rand number
-    var  dice = Math.floor(Math.random()*6) + 1;
+    var  dice1 = Math.floor(Math.random()*6) + 1;
+    var  dice2 = Math.floor(Math.random()*6) + 1;
     //2. Display result
-    var diceDom = document.querySelector('.dice');
-    diceDom.style.display = 'block';
-    diceDom.src = 'dice-' + dice + '.png';
+    document.getElementById('dice-1').style.display = 'block';
+    document.getElementById('dice-2').style.display = 'block';
+    document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+    document.getElementById('dice-1').src = 'dice-' + dice2 + '.png';
+    if(dice1 !== 1 && dice2 !== 1) {
+      //add score
+      roundScore += dice1 + dice2;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+      nextPlayer();
+    }
+    //challenge 2
+    /*
+    if( dice === 6 && lastDice ===6 ) {
+      // player looses score
+      scores[activePlayer] = 0;
+      document.querySelector('#score-' + activePlayer).textContent = '0';
+      nextPlayer();
+    }
     //3. Update the round score IF the rolled number was NOT 1
-    if(dice !== 1) {
+    else if(dice !== 1) {
       //add score
       roundScore += dice;
       document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
       nextPlayer();
     }
+
+    lastDice = dice;
+    */
   }
 });
 
@@ -42,11 +60,21 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     //Add current score to global score
   scores[activePlayer] += roundScore;
   //update the UI
-  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+  var input = document.querySelector('.final-score').value;
+  var winningScore
+  if (input) {
+    winningScore = input;
+  }
+  else {
+    winningScore = 100;
+  }
   //check if player won the game
-  if(scores[activePlayer] >= 10) {
+  if(scores[activePlayer] >= winningScore) {
     document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none ';
     document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner')
     document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active')
     gamePlaying = false;
@@ -54,7 +82,8 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
   else {
     nextPlayer();
   }
-  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('dice-1').style.display = 'block';
+  document.getElementById('dice-2').style.display = 'block';
   }
 });
 
@@ -71,6 +100,8 @@ document.querySelector('.player-0-panel').classList.toggle('active');
 document.querySelector('.player-1-panel').classList.toggle('active');
 // document.querySelector('.player-0-panel').classList.remove('active');
 // document.querySelector('.player-1-panel').classList.add('active');
+document.getElementById('dice-1').style.display = 'none';
+document.getElementById('dice-2').style.display = 'none';
 }
 
 function init(){
@@ -78,7 +109,8 @@ function init(){
   activePlayer = 0;
   roundScore = 0;
   gamePlaying = true;
-  document.querySelector('.dice').style.display = 'none'; // hide image
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
