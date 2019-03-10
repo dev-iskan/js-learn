1ff345730a028b03fde10874aff1d61c861065e7
dev-iskan <successfuliskan@gmail.com>
2019-03-10 15:38:10 +0500

    added products page fixed image showing, refactoring code

--- advanced/10-node-js/index.js
+++ advanced/10-node-js/index.js
@@ -16,7 +16,16 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
       'Content-type': 'text/html'
     })
-    res.end('This is Products page response')
+    
+    fs.readFile(`${__dirname}/templates/overview.html`, 'utf-8', (err, overviewData) => {
+      fs.readFile(`${__dirname}/templates/cart.html`, 'utf-8', (err, cartData) => {
+        // create array of carts in html and join by empty string
+        const cartOutputs = laptopData.map(el => replaceTemplate(cartData, el)).join('')
+        const overviewOuput = overviewData.replace(/{%CARDS%}/g, cartOutputs)
+        res.end(overviewOuput)
+      })
+    })
+
   } else if (pathName === '/laptop' && id < laptopData.length) {
     res.writeHead(200, {
       'Content-type': 'text/html'
@@ -24,17 +33,17 @@ const server = http.createServer((req, res) => {
     // async function to read file
     fs.readFile(`${__dirname}/templates/laptop.html`, 'utf-8', (err, readData) => {
       const laptop = laptopData[id]
-      let output = readData.replace(/{%PRODUCTNAME%}/g, laptop.productName)
-      output = output.replace(/{%IMAGE%}/g, laptop.image)
-      output = output.replace(/{%PRICE%}/g, laptop.price)
-      output = output.replace(/{%SCREEN%}/g, laptop.screen)
-      output = output.replace(/{%CPU%}/g, laptop.cpu)
-      output = output.replace(/{%STORAGE%}/g, laptop.storage)
-      output = output.replace(/{%RAM%}/g, laptop.ram)
-      output = output.replace(/{%DESCRIPTION%}/g, laptop.description)
-      res.end(output)
+      res.end(replaceTemplate(readData, laptop))
     })
-  } else {
+  } 
+  // image path need to get image from server, as node doesn't see filesystem as usual server
+  else if ((/\.(jpg|jpeg|png|gif)$/i).test(pathName)) {
+    fs.readFile(`${__dirname}/data/img/${pathName}`, (err, data) => {
+      res.writeHead(200, {'Content-type': 'image/jpg'})
+      res.end(data)
+    })
+  }
+  else {
     res.writeHead(404, {
       'Content-type': 'text/html'
     })
@@ -46,3 +55,18 @@ const server = http.createServer((req, res) => {
 server.listen(1337, '127.0.0.1', () => {
   console.log('Listening for requests')
 }) // listen to the server
+
+
+
+function replaceTemplate (originalHtml, laptop) {
+  let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName)
+  output = output.replace(/{%IMAGE%}/g, laptop.image)
+  output = output.replace(/{%PRICE%}/g, laptop.price)
+  output = output.replace(/{%SCREEN%}/g, laptop.screen)
+  output = output.replace(/{%CPU%}/g, laptop.cpu)
+  output = output.replace(/{%STORAGE%}/g, laptop.storage)
+  output = output.replace(/{%RAM%}/g, laptop.ram)
+  output = output.replace(/{%DESCRIPTION%}/g, laptop.description)
+  output = output.replace(/{%ID%}/g, laptop.id)
+  return output
+}
